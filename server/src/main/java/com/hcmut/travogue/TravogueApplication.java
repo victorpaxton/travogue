2d4a9931a74c8c4c8540e199089174fc574b0155
thangdoggy <hhqthang@gmail.com>
2023-11-24 00:41:33 +0700

    init project

---
+++ server/src/main/java/com/hcmut/travogue/TravogueApplication.java
@@ -0,0 +1,13 @@
+package com.hcmut.travogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravogueApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/hcmut/travogue/TravogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcmut.travogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
