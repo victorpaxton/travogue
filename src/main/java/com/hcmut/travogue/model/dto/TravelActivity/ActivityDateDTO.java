65c6c2032b113913437abd8fa54b83810ba81fae
thangdoggy <hhqthang@gmail.com>
2024-01-14 00:58:46 +0700

    fix type date

--- src/main/java/com/hcmut/travogue/model/dto/TravelActivity/ActivityDateDTO.java
+++ src/main/java/com/hcmut/travogue/model/dto/TravelActivity/ActivityDateDTO.java
@@ -1,11 +1,14 @@
 package com.hcmut.travogue.model.dto.TravelActivity;
 
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
 
 import java.util.Date;
 
 @Data
 public class ActivityDateDTO {
+    @Temporal(TemporalType.DATE)
     private Date date;
     private String hostNotes;
 }
