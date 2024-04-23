d8e57a2e115b5cd46aa6b19f52b5a9b4e3128fd2
thangdoggy <hhqthang@gmail.com>
2024-04-23 14:09:48 +0700

    fix: post response

--- src/main/java/com/hcmut/travogue/model/dto/Post/PostResponseDTO.java
+++ src/main/java/com/hcmut/travogue/model/dto/Post/PostResponseDTO.java
@@ -2,8 +2,11 @@ package com.hcmut.travogue.model.dto.Post;
 
 import com.hcmut.travogue.model.entity.TravelActivity.TravelActivity;
 import com.hcmut.travogue.model.entity.User.User;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.Data;
 
+import java.util.Date;
 import java.util.UUID;
 
 @Data
@@ -15,4 +18,8 @@ public class PostResponseDTO {
     private int numOfComments;
     private int numOfLikes;
     private TravelActivity travelActivity;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdAt;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedAt;
 }
